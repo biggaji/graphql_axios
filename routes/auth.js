fefd1efd40c1ecdd182c3e4a9c30937cb83507d9
Tobi Ajibade <herityjohnny14>
2020-11-19 15:59:36 -0800

    Dashboard controller working

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const { enter,signin } = require('../controller/auth');
+const { enter,signin, dashboardControl } = require('../controller/auth');
+const { verify } = require('../utils/verifyUser');
 
 router.get('/signin', signin);
 router.post('/enter',enter);
+router.get('/dashboard',verify, dashboardControl)
 
 module.exports = router;
