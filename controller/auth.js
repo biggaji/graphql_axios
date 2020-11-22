f664b09af64f7e6b359a7d6c711d2ca8c484f4bb
Tobi Ajibade <herityjohnny14>
2020-11-21 16:06:04 -0800

    Styled and re-arranged dashboard

--- controller/auth.js
+++ controller/auth.js
@@ -1,14 +1,14 @@
 const fetch = require('node-fetch');
-const {db} = require('../configs/db');
+const { db } = require('../configs/db');
 const jwt = require('jsonwebtoken');
 
 const API_ENDPOINT = "http://localhost:4000/graphql";
 
-exports.signin = async (req,res) => {
+exports.signin = async (req, res) => {
     res.render('signin');
 }
 
-exports.enter = async (req,res) => {
+exports.enter = async (req, res) => {
     // Get the data from
     // console.log(req.body)
     // res.send(req.body);
@@ -17,10 +17,11 @@ exports.enter = async (req,res) => {
         method: "post",
         headers: {
             'Content-type': 'application/json',
-            'Accept' : 'application/json'
+            'Accept': 'application/json'
         },
-        body: JSON.stringify({query:
-        `mutation { 
+        body: JSON.stringify({
+            query:
+                `mutation { 
             signin(username: "${username}"){
                     name
                     username
@@ -30,57 +31,57 @@ exports.enter = async (req,res) => {
             }`
         })
     })
-    .then(resp => {
-        return resp.json();
-    })
-    .then(data => {
-        if(data.data.signin === null) {
+        .then(resp => {
+            return resp.json();
+        })
+        .then(data => {
+            if (data.data.signin === null) {
+                res.redirect('/u/signin');
+            } else {
+                //sign user with jwt
+                const user = data.data.signin
+                // console.log(user)
+                const signeduser = jwt.sign({ authorid: user.authorid, username: user.username }, process.env.JWT_SECRET, { expiresIn: 3600 * 24 });
+
+                //store user in the cookie
+                res.cookie("x_user", signeduser, { maxAge: 86400, httpOnly: true });
+                res.cookie("isLoggedin", true);
+                res.clearCookie("isLoggedOut");
+                res.redirect('/u/dashboard');
+            }
+        })
+        .catch(e => {
             res.redirect('/u/signin');
-        } else {
-            //sign user with jwt
-            const user = data.data.signin
-            // console.log(user)
-            const signeduser = jwt.sign({authorid : user.authorid, username: user.username}, process.env.JWT_SECRET,{expiresIn : 3600 * 24});
-            
-            //store user in the cookie
-            res.cookie("x_user", signeduser, {maxAge : 86400, httpOnly: true});
-            res.cookie("isLoggedin", true);
-            res.clearCookie("isLoggedOut");
-            res.redirect('/u/dashboard');
-        }
-    })
-    .catch(e => {
-            res.redirect('/u/signin');
-    });
+        });
 }
 
 //Dashboard controller
 
-exports.dashboardControl = async (req,res) => {
+exports.dashboardControl = async (req, res) => {
     //get user data from the req.user
     const { authorid } = req.user;
 
     //find the user based on the authorid
 
     db.query('SELECT name, username, twitter_handle FROM author WHERE authorid = $1', [authorid])
-    .then(author => {
-        const authorData = author.rows[0];
-        res.render('dashboard', {user: authorData})
-    })
-    .catch(e => {
-        res.render('signin', {error: e})
-    })
+        .then(author => {
+            const authorData = author.rows[0];
+            res.render('dashboard', { user: authorData, pagename: "Dashboard" })
+        })
+        .catch(e => {
+            res.render('signin', { error: e })
+        })
 }
 
 //get Twitter handle
 
-exports.twit_handle = async (req,res) => {
-    res.render('addtwitter');
+exports.twit_handle = async (req, res) => {
+    res.render('addtwitter', { pagename: "Update Twitter" });
 }
 
 //Add twitter handle
 
-exports.add_twitter_handle = async (req,res) => {
+exports.add_twitter_handle = async (req, res) => {
     //get the twitter_handle fro req.body
     const { twitter_handle } = req.body;
     const { authorid } = req.user;
@@ -89,13 +90,13 @@ exports.add_twitter_handle = async (req,res) => {
     //Send the twitter_handle to the graphql server
 
     fetch(API_ENDPOINT, {
-        method : "post",
+        method: "post",
         headers: {
-            'content-type' : 'Application/json',
-            'Accept' : 'application/json',
+            'content-type': 'Application/json',
+            'Accept': 'application/json',
             'x_id': `${authorid}`
         },
-        body : JSON.stringify({
+        body: JSON.stringify({
             query: `
                 mutation {
                     addTwitterHandle(twitter_handle: "${twitter_handle}") {
@@ -104,22 +105,22 @@ exports.add_twitter_handle = async (req,res) => {
                 }
         `})
     })
-    .then(resp => {
-        return resp.json();
-    })
-    .then(twitter => {
-        if(twitter.data.addTwitterHandle === null) {
+        .then(resp => {
+            return resp.json();
+        })
+        .then(twitter => {
+            if (twitter.data.addTwitterHandle === null) {
+                res.redirect('/u/utwitter');
+            } else {
+                res.redirect('/u/dashboard');
+            }
+        })
+        .catch(e => {
+            console.log(e)
             res.redirect('/u/utwitter');
-        } else {
-        res.redirect('/u/dashboard');
-        }
-    })
-    .catch(e => {
-        console.log(e)
-        res.redirect('/u/utwitter');
-    })
+        })
     // const { authorid } = req.user;
-    
+
     //insert into db based on authorid
 
     // db.query('UPDATE author SET twitter_handle = $1 WHERE authorid = $2 RETURNING twitter_handle',[twitter_handle,authorid])
@@ -133,7 +134,7 @@ exports.add_twitter_handle = async (req,res) => {
 
 //Logout user 
 
-exports.logout = async (req,res) => {
+exports.logout = async (req, res) => {
     res.clearCookie("x_user");
     res.clearCookie("isLoggedin");
     res.cookie("isLoggedOut", true);
--- controller/index.js
+++ controller/index.js
@@ -2,13 +2,13 @@ const fetch = require('node-fetch');
 
 const api_url = 'http://localhost:4000/graphql';
 
-exports.indexControl = async (req,res) => {
+exports.indexControl = async (req, res) => {
     try {
         const loggeduser = await req.cookies.isLoggedin;
         const loggedOut = await req.cookies.isLoggedOut;
         // console.log(user)
-        if(loggedOut) {
-            res.render('index');
+        if (loggedOut) {
+            res.render('index', { pagename: "Web app" });
         } else if (loggeduser) {
             res.redirect('/u/dashboard');
         } else {
